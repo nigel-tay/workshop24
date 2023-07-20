2b691429e100b8a11d46307dfe9d3de05dcc88ce
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-07-20 14:54:03 +0800

    write updateOrderDetails method

--- src/main/java/sg/edu/nus/iss/workshop24/repository/OrderRepository.java
+++ src/main/java/sg/edu/nus/iss/workshop24/repository/OrderRepository.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import sg.edu.nus.iss.workshop24.model.Order;
+import sg.edu.nus.iss.workshop24.model.OrderDetails;
 
 @Repository
 public class OrderRepository {
@@ -26,7 +27,7 @@ public class OrderRepository {
             VALUES (?, ?, ?, ?, ?);
         """;
 
-    public boolean makeOrder(Order order) {
+    public boolean updateOrder(Order order) {
 
         Date orderDate = order.getOrderDate();
         String customerName = order.getCustomerName();
@@ -35,6 +36,19 @@ public class OrderRepository {
         Double tax = order.getTax();
         
         int result = jTemplate.update(UPDATE_ORDER_SQL, orderDate, customerName, shipAddress, notes, tax);
+
+        return result > 0 ? true: false;
+    }
+
+    public boolean updateOrderDetails(OrderDetails orderDetails) {
+
+        Integer orderId = orderDetails.getOrderId();
+        String product = orderDetails.getProduct();
+        Double unitPrice = orderDetails.getUnitPrice();
+        Double discount = orderDetails.getDiscount();
+        Integer quantity = orderDetails.getQuantity();
+        
+        int result = jTemplate.update(UPDATE_ORDER_DETAILS_SQL, orderId, product, unitPrice, discount, quantity);
 
         return result > 0 ? true: false;
     }
